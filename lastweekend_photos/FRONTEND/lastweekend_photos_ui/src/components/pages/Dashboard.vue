517072cbb6a683026a390d1ca1ae0e923f422748
Omid Farvid <OmidFarvid@gmail.com>
2019-05-21 02:01:03 +0430

    change some dashboard style

--- lastweekend_photos/FRONTEND/lastweekend_photos_ui/src/components/pages/Dashboard.vue
+++ lastweekend_photos/FRONTEND/lastweekend_photos_ui/src/components/pages/Dashboard.vue
@@ -32,53 +32,54 @@
             <div class="kt-section kt-section--first">
               <div class="kt-section__body">
                 <div class="form-group kt-form__group row">
-                  <div class="container">
+                  <div class="container kt-content">
                     <div class="row mt-3">
                       <div class="col-md-5 element-box mt-3" v-for="i in 4" v-bind:key="i">
-                        <div class="thumbnail">
-                          <div class="row">
-                            <div class="col-md-2 text-center">
-                              <span class="la la-user h2"></span>
-                            </div>
-                            <div class="col-md-10">
-                              <div class="row">
-                                <div class="col-md-12 text-left h3 text-black">
-                                  albert nic
+                        <div class="kt-portlet">
+                          <div class="kt-portlet__body">
+                            <div class="row">
+                              <div class="col-md-2 text-center">
+                                <span class="la la-user h2"></span>
+                              </div>
+                              <div class="col-md-10">
+                                <div class="row">
+                                  <div class="col-md-12 text-left h3 text-black">
+                                    albert nic
+                                  </div>
+                                </div>
+                                <div class="row">
+                                  <div class="col-md-12 text-left h3">
+                                    london
+                                  </div>
                                 </div>
                               </div>
-                              <div class="row">
-                                <div class="col-md-12 text-left h3">
-                                  london
-                                </div>
+                            </div>
+                            <div class="row">
+                              <div class="col-12 image-box">
+                                <img v-bind:src="'resources/images/'+'img'+i+'.jpg'"
+                                     style="width: 100%;height: auto;">
+                                <span class="cover-row">man in laps</span>
                               </div>
                             </div>
-                          </div>
-                          <div class="row">
-                            <div class="col-12 image-box">
-                              <img v-bind:src="'resources/images/'+'img'+i+'.jpg'"
-                                   style="width: 100%;height: auto;">
-                              <span class="cover-row">man in laps</span>
-                            </div>
-                          </div>
-                          <div class="row">
-                            <div class="col-md-2 text-center h5">
-                              <span class="la la-comment-o"></span>
-                              35
-                            </div>
-                            <div class="col-md-3 text-center h5">
-                              <span class="la la-heart-o"></span>
-                              443
-                            </div>
-                            <div class="col-md-1 offset-3 h3">
-                              <span class="la la-star-o"></span>
-                            </div>
-                            <div class="col-md-1 h3">
-                              <span class="la la-thumbs-o-up"></span>
-                            </div>
-                            <div class="col-md-1 h3" style="font-weight: bold">
-                              <a is="router-link" :to="{name: $rns.DOWNLOAD}" class="kt-menu__link ">
-                                <span class="la la-download"></span>
-                              </a>
+                            <div class="row mt-2">
+                              <div class="col-md-2 h4">
+                                  <span class="la la-comment kt-font-success">35</span>
+                                </div>
+                              <div class="col-md-2 h4">
+                                <span class="la la-heart kt-font-danger">443</span>
+                              </div>
+                              <div class="col-md-4 offset-4 d-flex h3">
+                                <a class="btn btn-sm btn-icon btn-brand btn-elevate btn-icon-md text-white mr-1">
+                                  <span class="la la-star"></span>
+                                </a>
+                                <a class="btn btn-sm btn-icon btn-danger btn-elevate btn-icon-md text-white mr-1">
+                                  <span class="la la-thumbs-up"></span>
+                                </a>
+                                <a is="router-link" :to="{name: $rns.DOWNLOAD}"
+                                   class="btn btn-sm btn-icon btn-success btn-elevate btn-icon-md text-white mr-1">
+                                  <span class="la la-download"></span>
+                                </a>
+                              </div>
                             </div>
                           </div>
                         </div>
@@ -116,13 +117,13 @@
 </template>
 
 <script>
-    import PageBar from "../partials/PageBar";
+  import PageBar from "../partials/PageBar";
 
-    export default {
-        components: {
-            PageBar
-        }
+  export default {
+    components: {
+      PageBar
     }
+  }
 </script>
 
 <style scoped>
@@ -137,6 +138,10 @@
   }
 
   .nav-tabs.nav-tabs-line.nav-tabs-line-brand.nav-tabs-line-2x {
-    border: none !important;;
+    border: none !important;
+  }
+
+  .kt-portlet{
+    border:1px solid #eeeeee;
   }
 </style>
